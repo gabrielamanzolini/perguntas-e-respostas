0f94a2ea5caa422a6faaac730afac15da862a475
Gabriela Manzolini <gabi.manzolini@gmail.com>
2019-02-01 11:03:42 -0200

    Update class 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Inicio from './Home/inicio'
 
 class App extends Component {
   render() {
@@ -10,9 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Jogo de Perguntas e Respostas</h1>
         </header>
-        <p className="App-intro">
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Inicio/>
       </div>
     );
   }
