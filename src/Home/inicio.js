1ffff47c638c3b67dcc029c567f96037226541da
Gabriela Manzolini <gabi.manzolini@gmail.com>
2019-02-01 11:32:45 -0200

    Update home page component

--- src/Home/inicio.js
+++ src/Home/inicio.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import {Container, Card, Button, Header, Segment} from 'semantic-ui-react'
 
 const Inicio = props => {
     return (
         <div>
-            <h1>Bem vindo!</h1>
-            <p>Este é um jogo de perguntas e respostas</p>
+            <Container>
+                <Segment pilled>
+                    Quiz
+                </Segment>
+                <Header as='h2'>
+                    Jogo de Perguntas e Respostas
+                </Header>
+                <p>Desafie seus amigos neste jogo</p>
+                <Card fluid>
+                    <Card.Content>
+                        Acesse agora mesmo:
+                    </Card.Content>
+                    <Card.Content>
+                        <Button basic color='blue'>Login com Facebook</Button>
+                        <Button basic color='gree'>Usuário Administrativo</Button>
+                    </Card.Content>
+                </Card>
+            </Container> 
         </div>
     )
 }
