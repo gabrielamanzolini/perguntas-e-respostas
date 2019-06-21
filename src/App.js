f8b09c1fad0c398fcaef9bf47ab341787399f3f5
Gabriela Manzolini <gabi.manzolini@gmail.com>
2019-06-21 14:35:43 -0300

    Criação de componente de pergunta

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import Inicio from './Home/inicio'
-import Categorias from './Jogo/categorias'
+import Inicio from './Home/inicio';
+import Categorias from './Jogo/categorias';
+import Perguntas from "./Jogo/perguntas";
 
 class App extends Component {
   render() {
@@ -12,7 +13,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Jogo de Perguntas e Respostas</h1>
         </header>
-        <Categorias/>
+        <Perguntas/>
       </div>
     );
   }
